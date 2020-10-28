6617bba008719c91e1f67b4b4cc6cf925f8a9f82
JCarlosVico <jfervic933@maralboran.es>
2020-10-28 13:44:08 +0100

    Modificacíon del archivo

--- src/main/java/jcarlos/valoresaleatorios/Ejemplos.java
+++ src/main/java/jcarlos/valoresaleatorios/Ejemplos.java
@@ -20,7 +20,7 @@ public class Ejemplos {
 
         // El método nextInt(int valor) genera un int aleaotorio entre 0 y 
         // valor, exluyendo valor
-        enteroAleatorio = random.nextInt(100);
+        enteroAleatorio = random.nextInt(100); // 0 - 99
         System.out.println("Entero aleatorio (0-99): " + enteroAleatorio);
 
         // De la siguiente forma, obtenemos un número entre 1 y valor
@@ -40,9 +40,10 @@ public class Ejemplos {
         // random.ints(cantidad_numeros_a_generar) devuelve un IntStream de
         // tamaño cantidad_numeros_a_generar. Con el método toArray() se 
         // guardan en un array de ese tamaño
-        int tam = 15; // 15 enteros aleatorios dentro del rango de los int
+        int tam = 10; // 15 enteros aleatorios dentro del rango de los int
         int[] intAleatorios = random.ints(tam).toArray();
 
+        // array.length me da el tamaño del array
         for (int i = 0; i < intAleatorios.length; i++) {
             System.out.print(intAleatorios[i] + " # ");
         }
@@ -50,7 +51,8 @@ public class Ejemplos {
         System.out.println("\b\b");
 
         // Se puede limitar el rango, por ejemplo entre -30 y 30
-        intAleatorios = random.ints(tam, -30, 30).toArray();
+        // Rango real es entre -30 y 29
+        intAleatorios = random.ints(tam, -3, 3).toArray();
 
         for (int i = 0; i < intAleatorios.length; i++) {
             System.out.print(intAleatorios[i] + " # ");
@@ -76,6 +78,8 @@ public class Ejemplos {
         // Otra opción es usando un String y el método charAt
         final String LETRAS = "abcdefghijklmnñopqrstuvwxyz";
         int indiceLetraSacar = random.nextInt(LETRAS.length());
+        // El método charAt devuelve un char del String que ocupa la posición
+        // indice
         char letra = LETRAS.charAt(indiceLetraSacar);
         System.out.println("Letra sacada: " + letra);
     }
